db04c7d8651724d973b96511bdd889454d6ac19c
helbertmoreirapinto <helbert.tec.info@gmail.com>
2020-03-24 19:55:11 -0300

    modify syscall process

--- Trabalho1/Parte1/Syscall_Process.c
+++ Trabalho1/Parte1/Syscall_Process.c
@@ -2,18 +2,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define SECOND 60
+
 int main(void) {
 	int pid;
-	int i = 0;
+	int i = 6;
+
 	pid = fork();
+	pid_t process = getpid();
 
 	if (!pid) {
-		i++;
-		printf("Processo pai i[%d]\n", i);
+		i += 3;
+		printf("--------\n");
+		printf("Somente processo pai ID[%d] passa por este trecho\n", pid);
+		printf("Thread pai vai dormir %d segundos\n", i);
+		sleep(i);
+
 	} else {
-		printf("Processo filho id[%d] i[%d]\n", pid, i);
+		i -= 3;
+		printf("--------\n");
+		printf("Somente processo filho ID[%d] passa por este trecho\n", pid);
+		printf("Thread filho vai dormir %d segundos\n", i);
+		sleep(i);
 	}
-	printf("AQUI\n");
+
+	printf("Programa ID[%d] vai finalizar\n", pid);
+
 
 	return EXIT_SUCCESS;
 }
