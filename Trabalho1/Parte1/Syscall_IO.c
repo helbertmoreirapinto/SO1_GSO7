65e46129378d0b074bf6844a7c88b4698d975972
Helbert <helbert.tec.info@gmail.com>
2020-03-25 20:09:04 -0300

    syscall IO ok

--- Trabalho1/Parte1/Syscall_IO.c
+++ Trabalho1/Parte1/Syscall_IO.c
@@ -1,81 +1,78 @@
 /*
  ============================================================================
- Title: Program 1
  Description  : This is an example program that uses I/O in both standard and file input.
- Syscalls uses: open()
- read()
- write()
+ Syscalls uses:
+ open()  - 
+ creat() - 
+ write() - 
+ read()  - 
  ============================================================================
  */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+ 
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-/*---------------------------- FUNCTIONS ----------------------------*/
-const char* read_standard_input();
-void write_standard_input(const char*);
-const char* read_file_input();
-void write_file_input(const char*);
+#define ERROR -1
+#define MAX_BUFFER 50
 
 /*------------------------------ MAIN -------------------------------*/
 int main() {
+	
+	/* String pool */
+	char name_file[] = "fileExampl.out";
+	char msg_file[] = "text for example abc123\n\0";
+	
+	char msg_err1[] = "File not found\n\0";
+	char msg_err2[] = "Trying create file\n\0";
+	char msg_err3[] = "Failed to create file\n\0";
+	char msg_err4[] = "Failed to open file\n\0";
 
-	char text[] = "text example\0";
+	char msg_inf1[] = "Sucess!!!\n\0";
+	char msg_inf2[] = "Writting in file\n\0";
+	char msg_inf3[] = "Open file for write\n\0";
+	char msg_inf4[] = "Open file for read\n\0";
+	
+	int ptr_file = open(name_file, O_WRONLY);
+	
+	if(ptr_file == ERROR){
+		write(0, msg_err1, strlen(msg_err1));
 
-//	read_standard_input();
-
-	write_standard_input(text);
-
-//	read_file_input();
-
-//	write_file_input(text);
-
-	return EXIT_SUCCESS;
-}
-
-/*---------------------------- FUNCTIONS ----------------------------*/
-const char* read_standard_input() {
-	int ret, nBytes = 10;
-	char buffer[nBytes];
-
-	while (true) {
-		ret = read(0, buffer, nBytes - 1);
-		buffer[ret] = '\0';
-		if (ret < (nBytes - 1)) {
-			printf("%s", buffer);
-			break;
-		} else {
-			printf("%s\n", buffer);
+		write(0, msg_err2, strlen(msg_err2));
+		ptr_file = creat(name_file, 0755);
+		if(ptr_file == ERROR){
+			write(0, msg_err3, strlen(msg_err3));
+			return ERROR;
 		}
+
+		write(0, msg_inf1, strlen(msg_inf1));
 	}
-	return NULL;
-}
 
-const char* read_file_input() {
-	int ret, nBytes = 10;
-	char buffer[nBytes];
+	write(0, msg_inf3, strlen(msg_inf3));
 
-	while (true) {
-		ret = read(0, buffer, nBytes - 1);
-		buffer[ret] = '\0';
-		if (ret < (nBytes - 1)) {
-			printf("%s", buffer);
-			break;
-		} else {
-			printf("%s\n", buffer);
-		}
+	write(ptr_file, msg_file, MAX_BUFFER);
+	
+	write(0, msg_inf2, strlen(msg_inf2));
+
+	close(ptr_file);
+
+	char buffer[MAX_BUFFER];
+
+	ptr_file = open(name_file, O_RDONLY);
+	if(ptr_file == ERROR){
+		write(0, msg_err4, strlen(msg_err4));
+		return ERROR;
 	}
-	return NULL;
-}
 
-void write_standard_input(const char* text) {
-	int tam = strlen(text);
-	write(0, text, tam);
-}
+	write(0, msg_inf4, strlen(msg_inf4));
 
-void write_file_input(const char* text) {
+	read(ptr_file , buffer, MAX_BUFFER);
 
+	write(0, buffer, strlen(buffer));
+
+	close(ptr_file);
+
+	return 0;
 }
