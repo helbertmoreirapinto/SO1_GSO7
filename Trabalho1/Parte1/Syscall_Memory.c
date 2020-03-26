4eb01d047a6018614e1ed8d24ab6b7d25bb7339f
Helbert <helbert.tec.info@gmail.com>
2020-03-26 13:36:40 -0300

    syscall mem ok

--- Trabalho1/Parte1/Syscall_Memory.c
+++ Trabalho1/Parte1/Syscall_Memory.c
@@ -1,43 +1,63 @@
 /*
  ============================================================================
- Description  : This is an example program that uses I/O in both standard and file input.
+ Description  : This example shows the difference in the use of arrays with
+                and without shared memory.
  Syscalls uses:
- brk() - 
- assert() - 
+ brk() -    Allocates memory for the process.
+ ftok() -   Return a unique key.
+ shmget() - Returns an identifier for the shared memory segment.
+ shmat() -  Connects to the memory segment.
  ============================================================================
  */
 
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <unistd.h> 
+#include <sys/ipc.h> 
+#include <sys/shm.h>
 
 int main() {
-
-    int tam_word = 15;
-    void* ptr_void = sbrk(0);
-    char* word = (char*) ptr_void;
-
-    brk(word + tam_word);
+    int arr_size = 15;
     
-    for(int i = 0; i < tam_word; i++)
-        printf("%d\n", word[i]);
-
-    strcpy(word, "teste");
+    //ARRAY 1 - Unshared memory
+    void* ptr_void1 = sbrk(0);
+    if(ptr_void1 == NULL){
+        printf("Error alocate memory [arr1]\n");
+        return 1;
+    }
+    int* arr1 = (int*) ptr_void1;
+    brk(arr1 + arr_size);
     
-    for(int i = 0; i < tam_word; i++)
-        printf("%d\n", word[i]);
+    //ARRAY 2 - Shared memory
+    key_t key = ftok("shmkey", 65);
+    int shmid = shmget(key, arr_size * sizeof(int), 0666 | IPC_CREAT);
 
-    assert(!strcmp(word, "teste"));
-    assert(word[0] == 't');
-    assert(word[1] == 'e');
-    assert(word[2] == 's');
-    assert(word[3] == 't');
-    assert(word[4] == 'e');
-    assert(word[5] == '\0');
+    if(shmid < 0){
+        printf("Error alocate memory [arr2]\n");
+        return 1;
+    }
+    
+    int pid = fork();
 
-    printf("%s\n", word);
-    brk(ptr_void);
+    int* arr2 = (int*) shmat(shmid, (void*)0,0); 
+
+    if(pid){
+        //Process 1 [Write data]
+        for(int i = 0; i < arr_size; i++)
+            arr1[i] = arr2[i] = i;
+    }else{
+        //Process 2 [Read data]
+        printf("Unshared array:\n");
+        for(int i = 0; i < arr_size; i++)
+            printf("%02d ", arr1[i]);
+        printf("\n\n");
+        printf("Shared array:\n");
+        for(int i = 0; i < arr_size; i++)
+            printf("%02d ", arr2[i]);
+        printf("\n");
+        
+    }
 
     return 0;
 }
