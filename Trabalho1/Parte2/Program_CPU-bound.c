18609591473651942e1df0d1381674605152c6db
Helbert <helbert.tec.info@gmail.com>
2020-03-27 12:18:15 -0300

    bug fix cpu bound

--- Trabalho1/Parte2/Program_CPU-bound.c
+++ Trabalho1/Parte2/Program_CPU-bound.c
@@ -2,28 +2,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(void) {
-	FILE* input = fopen("input.in", "r");
-	if (!input) {
-		input = stdin;
-	}
-
-	int range_start, range_end;
+int main() {
+	int range_start = 5000;
+	int range_end   = 30000;
 	bool prime_numb;
-
-	fscanf(input, "%d %d", &range_start, &range_end);
 
 	for (int i = range_start; i < range_end; i++) {
 		prime_numb = true;
 		for (int j = 2; j < i; j++) {
 			if (!(i % j)) {
 				prime_numb = false;
-				break;
+		//		break;
 			}
 		}
 
-		if (prime_numb)
-			printf("%d\n", i);
+		//if (prime_numb)
+		//	printf("%d\n", i);
 	}
 	return EXIT_SUCCESS;
 }
