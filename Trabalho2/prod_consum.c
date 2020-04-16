34e959339f926dbacc98bf896028189d9aee5bf4
Helbert <helbert.tec.info@gmail.com>
2020-04-16 20:43:02 -0300

    starts threads

--- Trabalho2/prod_consum.c
+++ Trabalho2/prod_consum.c
@@ -2,6 +2,34 @@
 #include<stdlib.h>
 #include<pthread.h>
 
+#define MAX_ITEMS  15
+#define MAX_BUFFER 3
+
+void* func_producer(void);
+void* func_consumer(void);
+
 int main(){
+    pthread_t th_prod;
+    pthread_t th_cons;
+
+    if (pthread_create(&th_prod, 0, (void*) func_producer, (void*) 0)) { 
+		printf("Erro ao criar produtor\n");
+		exit(0); //exits if error
+	}
+	if (pthread_create(&th_cons, 0, (void*) func_consumer, (void*) 0)) { 
+		printf("Erro ao criar consumidor\n");
+		exit(0); //exits if error
+	}
+
+    pthread_join(th_prod, 0);
+	pthread_join(th_cons, 0);
     return 0;
+}
+
+void* func_producer(void){
+    pthread_exit(0);
+}
+
+void* func_consumer(void){
+    pthread_exit(0);
 }
