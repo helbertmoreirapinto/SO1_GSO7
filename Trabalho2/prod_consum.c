88f93527c7ea1b25bc30d140d2cb2ca66fcb7778
Helbert <helbert.tec.info@gmail.com>
2020-04-16 20:52:28 -0300

    threads ok

--- Trabalho2/prod_consum.c
+++ Trabalho2/prod_consum.c
@@ -7,6 +7,15 @@
 
 void* func_producer(void);
 void* func_consumer(void);
+void produce_item(int item);
+int consume_item();
+void process_item(int item);
+
+pthread_cond_t cond_buffer_empty, cond_buffer_full;
+pthread_mutex_t atomic_process_lock;
+int buffer[MAX_BUFFER];
+int idx_buffer; //semaphore
+int produced_items, consumed_items;
 
 int main(){
     pthread_t th_prod;
@@ -27,9 +36,42 @@ int main(){
 }
 
 void* func_producer(void){
+    while(produced_items < MAX_ITEMS){
+        pthread_mutex_lock(&atomic_process_lock);
+        while (idx_buffer == MAX_BUFFER)
+            pthread_cond_wait(&cond_buffer_empty, &atomic_process_lock);
+        produce_item(123);
+        pthread_cond_signal(&cond_buffer_full);
+        pthread_mutex_unlock(&atomic_process_lock);
+    }
     pthread_exit(0);
 }
 
 void* func_consumer(void){
+    int elem;
+    while(consumed_items < MAX_ITEMS){
+        pthread_mutex_lock(&atomic_process_lock);
+        while (!idx_buffer)
+            pthread_cond_wait(&cond_buffer_full, &atomic_process_lock);
+        elem = consume_item();
+        pthread_cond_signal(&cond_buffer_empty);
+        pthread_mutex_unlock(&atomic_process_lock);
+        process_item(elem);
+    }
     pthread_exit(0);
+}
+
+void produce_item(int item){
+    produced_items++;
+    buffer[idx_buffer++] = item;
+}
+
+int consume_item(){
+    consumed_items++;
+    return buffer[--idx_buffer];
+}
+
+void process_item(int item){
+    static int processed_item = 0;
+    printf("processed item:%02d value:%d\n", ++processed_item, item);
 }
