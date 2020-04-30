098aeb98e92b052ecd8c2674bd715758cac7ac69
Helbert <helbert.tec.info@gmail.com>
2020-04-30 01:42:36 -0300

    uncomment sleeptime

--- Trabalho2/prod_consum_stack.c
+++ Trabalho2/prod_consum_stack.c
@@ -59,7 +59,7 @@ void* func_producer(void){
     int item;
     while(produced_items < MAX_ITEMS){
         item = random_item();
-        //usleep(100 * MILLISEC);
+        usleep(200 * MILLISEC);
 
         //------BLOCK CRITICAL REGION------//
         pthread_mutex_lock(&atomic_process_lock);
@@ -81,7 +81,7 @@ void* func_producer(void){
 void* func_consumer(void){
     int item;
     while(consumed_items < MAX_ITEMS){
-        //usleep(500 * MILLISEC);
+        usleep(120 * MILLISEC);
 
         //------BLOCK CRITICAL REGION------//
         pthread_mutex_lock(&atomic_process_lock);
