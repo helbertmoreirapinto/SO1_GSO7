5a76c58daaecf651e6222ce2926af0de5f655547
Helbert <helbert.tec.info@gmail.com>
2020-06-01 19:47:50 -0300

    init trab3

---
+++ Trabalho3/process.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*Resources*/
+#define PRI_MEMORY 1024 /*Kbytes*/
+#define SEC_MEMORY 2048 /*Kbytes*/
+#define SIZE_PAGE 4     /*Kbytes*/
+
+class Disc_Page{
+public:
+
+};
+
+int main(){
+    
+    //create pages im memories() //format()
+    //create virtualization()
+    
+    //create process()
+    //allocate memory for process()
+    //use memory
+    //kill process()
+
+    //kill pages() //free memory
+    return 0;
+}
