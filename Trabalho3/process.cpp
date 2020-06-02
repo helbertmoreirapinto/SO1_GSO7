1dbc0f10a6a41b09c50c535ab9a62703c1179358
Gabriell <gabrielltavsb@gmail.com>
2020-06-02 18:08:54 -0300

    add via GitKraken

--- Trabalho3/process.cpp
+++ Trabalho3/process.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 
 /*Resources*/
+#define VIR_MEMORY 2048 /*Kbytes*/
 #define PRI_MEMORY 1024 /*Kbytes*/
 #define SEC_MEMORY 2048 /*Kbytes*/
 
+
 class Disc_Page{
-public:
-    uint16_t ID;
-    uint16_t size;
-    Disc_Page* reference;
+    public:
+        uint16_t ID;
+        uint16_t size;
+        Disc_Page* reference;
 
     Disc_Page(uint16_t size, uint16_t ID){
         this->ID = ID;
@@ -18,6 +20,60 @@ public:
         this->reference = NULL;
     }
 
+    string toString(){
+        return "P";// + this->ID;
+    }
+
+};
+
+class Process{
+    public:
+        uint16_t ID;
+        uint16_t size;
+        uint16_t len_pages;
+        vector<Disc_Page> pages;
+        bool allocated = false; // process at principal memory
+    
+    Process(uint16_t ID, uint16_t size, uint16_t size_page){
+        this->ID = ID;
+        this->size = size;
+        this->len_pages = (int)(size /size_page) + 1;
+        for(int i = 0; i < len_pages; i++){
+            Disc_Page page(size_page, i);
+            pages.push_back(page);
+        }
+
+    }
+
+    bool read(uint16_t adrs){
+        return this->allocated;
+    }
+};
+
+class Virtual_Memory{
+    public:
+        uint16_t size;
+        uint16_t page_size;
+        uint16_t pages_total;
+        uint16_t pages_allocated = 0;
+        vector<Process> process_table;
+    
+    Virtual_Memory(uint16_t size, uint16_t page_size){
+        this->size = size;
+        this->page_size = page_size;
+        this->pages_total = (int)(size/page_size);
+    }
+
+    bool allocate_process(Process p){
+        if(this->pages_allocated + p.len_pages < this->pages_total){
+            process_table.push_back(p);
+            p.allocated = true;
+            this->pages_allocated += p.len_pages;
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 void format(uint16_t size_page);
@@ -32,10 +88,20 @@ int main(){
     uint16_t size_page;
     cout << "Input size disc page : "; cin >> size_page;
 
-    format(size_page);
-    init_virt();
+    Virtual_Memory vm(VIR_MEMORY, size_page);
+    cout << "Paginas total: " << vm.pages_total << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        Process p(i, 1023, size_page);
+        cout << vm.allocate_process(p) << endl;
+        cout << "Paginas alocadas: " << vm.pages_allocated << endl;
+    }
+    
+    
+    //format(size_page);
+    //init_virt();
 
-    print_mem();
+    //print_mem();
 
     //create pages im memories() //format()
     //create virtualization()
@@ -64,9 +130,6 @@ void format(uint16_t size_page){
     }
 }
 
-void init_virt(){
-
-}
 
 void print_mem(){
     cout << "Principal Memory\n";
